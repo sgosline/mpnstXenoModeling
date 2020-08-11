e2caaa8c60706eee439da13c37a6ae51d5307c98
Sara Gosline <sara.gosline@pnnl.gov>
2020-08-11 07:22:15 -0700

    Just added some things to make plotting easier!

--- results/ttest_MPNST.R
+++ results/ttest_MPNST.R
@@ -17,6 +17,9 @@ df = MXM::querySynapseTable("syn22279826")
 #independent t test
 community = df %>% filter(net2_type == 'community')
 
+##added this to help plotting
+community$category = rep('Tumor',nrow(community))
+community$category[grep('xenograft',community$net1)]<-'Xenograft'
 
 ttest_comm = function(x){
   data = community %>% filter(net2 == x)
@@ -29,9 +32,18 @@ ttest_comm = function(x){
 
 community_list = unique(community$net2)
 #models = rep(0,45)
-for (val in community_list){
-  print(paste("Community number", val))
-  model = ttest_comm(val)
-  print(model)
-}
+#for (val in community_list){
+#  print(paste("Community number", val))
+#  model = ttest_comm(val)
+#  print(model)
+#}
+
+pvals<-community_list%>%
+  purrr::map(~ttest_comm(.)$p.value)%>%as.numeric()%>%unlist()
+
+#now adding larger data frame to plot
+res.df<-data.frame(Community=unlist(community_list),PValue=pvals)
+community <- community%>%rename(Community=net2)%>%left_join(res.df)
+
+ggplot(subset(community,PValue<0.3),aes(x=Community,y=distance,fill=category))+geom_boxplot()
 
