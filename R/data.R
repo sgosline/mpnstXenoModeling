118af322659deb58614df70a2b11c621f2ac62f1
Sara Gosline <sara.gosline@pnnl.gov>
2021-09-13 08:19:08 -0700

    new figures added

--- R/data.R
+++ R/data.R
@@ -461,14 +461,16 @@ getMicroTissueDrugData <- function(syn, mtd) {
 
   res=do.call(rbind,lapply(names(indiv),function(x)
   {
-    print(x)
+   # print(x)
     tab<-read.csv(syn$get(x)$path,fileEncoding = 'UTF-8-BOM')
-    print(head(tab))
+   # print(head(tab))
     tab%>%
     dplyr::select(DrugCol='compound_name', CellLine='model_system_name', Conc='dosage',
                   Resp='response', RespType='response_type', ConcUnit='dosage_unit') %>%
     tidyr::pivot_wider(names_from=RespType, names_sep='.', values_from=Resp) %>%
     dplyr::rename(Viabilities='percent viability')
   }))
+  #res$`total cell count`=unlist(res$`total cell count`)
+  #res$`live cell count`=unlist(res$`live cell count`)
   return(res)
 }
