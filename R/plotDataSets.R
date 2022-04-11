1689157d6170049fa5604807e4092baf36289797
Sara Gosline <sara.gosline@pnnl.gov>
2022-04-11 15:38:51 -0700

    updated

--- R/plotDataSets.R
+++ R/plotDataSets.R
@@ -9,7 +9,7 @@ plotSingleGene<-function(gene='CD274'){
   dotplot<-subset(all.dat,Symbol==gene)%>%ggplot()+geom_point(aes(x=totalCounts,y=zScore,col=tumorType,shape=studyName))
   barplot<-subset(all.dat,Symbol==gene)%>%ggplot()+geom_boxplot(aes(x=studyName,y=totalCounts,fill=tumorType))
   cowplot::plot_grid(dotplot,barplot,nrow=2)
-  ggsave(paste(gene,'expression.png',sep=''))
+  ggplot2::ggsave(paste(gene,'expression.png',sep=''))
 }
 
 
@@ -20,14 +20,14 @@ plotSingleGene<-function(gene='CD274'){
 #' @import ggplot2
 #' @param drugData table of drug data to be plotted
 plotDrugData<-function(drugData){
-  library(ggplot2)
-  library(ggridges)
-  p<-ggplot(drugData,aes(x=as.numeric(volume),fill=drug,y=individualID))+
-    geom_density_ridges(alpha=0.5)+  scale_y_discrete(expand = c(0.01, 0)) +
-    scale_x_continuous(expand = c(0.01, 0)) +
-    scale_fill_viridis_d
+  #library(ggplot2)
+  #library(ggridges)
+  p<-ggplot2::ggplot(drugData,aes(x=as.numeric(volume),fill=drug,y=individualID))+
+    ggridges::geom_density_ridges(alpha=0.5)+  scale_y_discrete(expand = c(0.01, 0)) +
+    ggplot2::scale_x_continuous(expand = c(0.01, 0)) +
+    ggplot2::scale_fill_viridis_d
   print(p)
-  ggsave("allDrugVolumeScores.png")
+  ggplot2::ggsave("allDrugVolumeScores.png")
   p
 }
 
